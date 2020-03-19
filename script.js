f8c75cf34efbda4d54fc138152d6d64c19aba169
Kyuoraku <nico.99.godoy@gmail.com>
2020-03-19 17:02:21 -0300

    Falta agregar contrastes al texto

---
+++ script.js
@@ -0,0 +1,11 @@
+const sumar = () => {
+    var numero = document.getElementById('number')
+    numero.innerHTML = parseInt(numero.innerHTML) + 1
+}
+
+function restar(){
+
+    var numero = document.getElementById('number')
+    numero.innerHTML = parseInt(numero.innerHTML) - 1
+
+}
